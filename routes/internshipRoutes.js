3e662a738b27c94f53c348fe0c13b3646a790223
Samson Kwizela <kizelasamson@gmail.com>
2026-04-20 10:24:03 -0700

    added an internshipcontroller  model end point

---
+++ routes/internshipRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  createInternship,
+  getInternships,
+  deleteInternship,
+} = require('../controllers/internshipController');
+
+router.post('/', createInternship);
+router.get('/', getInternships);
+router.delete('/:id', deleteInternship);
+
+module.exports = router;
