79c18f1a1b0652f31867858af1360feae13db72d
Desire <desireirankundawilliams@gmail.com>
2026-05-25 20:14:43 +0200

    able to connect to the db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/scholarlink");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
